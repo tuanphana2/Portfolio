d2250b25f9eed6628e4dadb1952fdfcb1823d055
PQT <tuanphana202@gmail.com>
2025-03-30 12:33:24 +0700

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import AOS from "aos";
-import React, { useEffect } from "react";
-import { RouterProvider } from "react-router-dom";
+import AOS from 'aos';
+import React, { useEffect } from 'react';
+import { RouterProvider } from 'react-router-dom';
 
-import { router } from "./Router/Router.jsx";
+import { router } from './Router/Router.jsx';
 
-import "aos/dist/aos.css";
+import 'aos/dist/aos.css';
 
 const App = () => {
   useEffect(() => {
     AOS.init();
+    const handleUnload = () => {
+      const section = document.querySelector('section');
+      if (section) {
+        section.remove();
+      }
+    };
+
+    window.addEventListener('beforeunload', handleUnload);
+    return () => window.removeEventListener('beforeunload', handleUnload);
   }, []);
 
   return (
